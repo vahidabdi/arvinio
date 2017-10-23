c929f5c7137068cb6005bab6d59061e3509106ba
Vahid Abdi <vahidabdinet@gmail.com>
2017-10-23 11:06:42 +0330

    Add avatar

--- src/components/dashboard/header.js
+++ src/components/dashboard/header.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import {
   Container,
-  Segment,
   Menu,
-  Image
+  Image,
 } from 'semantic-ui-react';
 
 const Header = ({ currentUser }) => (
-    <Menu inverted pointing size='huge' fixed='top'>
-      <Container>
-        <Menu.Item as={NavLink} exact to='/dashboard' name='داشبورد' />
-        <Menu.Item as={NavLink} to='/dashboard/survey/new' name='پرسشنامه' />
-        <Menu.Menu position='right'>
-          <Menu.Item>
-            <Image src={currentUser.picture} avatar />
-            <span>{currentUser.email}</span>
-          </Menu.Item>
-        </Menu.Menu>
-      </Container>
-    </Menu>
-)
+  <Menu inverted pointing size="huge" fixed="top">
+    <Container>
+      <Menu.Item as={NavLink} exact to="/dashboard" name="داشبورد" />
+      <Menu.Item as={NavLink} to="/dashboard/survey/new" name="پرسشنامه" />
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Image src={currentUser.picture} avatar />
+          <span>{currentUser.email}</span>
+        </Menu.Item>
+      </Menu.Menu>
+    </Container>
+  </Menu>
+);
+
+Header.propTypes = {
+  currentUser: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Header;
